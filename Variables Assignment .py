f9b52190eaf9f3bcf6facc20daa6f4188e80c183
MLebron1015 <mlebron20@mail.valenciacollege.edu>
2025-01-26 20:00:06 -0500

    Add files via upload

---
+++ Variables Assignment .py
@@ -0,0 +1,31 @@
+value1 = int(input("multiply\n"))
+value2 = int(input("by\n"))
+sum =value1 * value2
+print("The answer to your query is {}".format(sum))
+
+value1 = int(input("divide\n"))
+value2 = int(input("by\n"))
+sum =value1/value2
+print("The answer to your query is {}".format(sum))
+
+value1 = int(input("divide\n"))
+value2 = int(input("by\n"))
+sum =value1//value2
+print("The answer to your query is {}".format(sum))
+
+value1 = int(input("Modulus\n"))
+value2 = int(input("by\n"))
+sum =value1 % value2
+print("The answer to your query is {}".format(sum))
+
+value1 = int(input("Add\n"))
+value2 = int(input("by\n"))
+sum =value1 + value2
+print("The answer to your query is {}".format(sum))
+
+value1 = int(input("Subtract\n"))
+value2 = int(input("by\n"))
+sum =value1 - value2
+print("The answer to your query is {}".format(sum))
+
+print("Marc Lebron")
