0caeeb4d3490d76549c174537c8fa6ea9a997f6e
MLebron1015 <mlebron20@mail.valenciacollege.edu>
2025-02-02 22:11:25 -0500

    Add files via upload

---
+++ Branching Assignment.py
@@ -0,0 +1,41 @@
+value1 = int(input("KW hours used:\n"))
+Value2 = .07633
+Value3 = 1000
+if value1 > 1000:
+    sum = ((value1 - Value3)*.09259) + (Value2 * Value3)
+else:
+    sum = value1 * Value2
+sum2 = (round(sum, 2))
+print("Amount owed is ${}".format(sum2))
+
+value1 = int(input("KW hours used:\n"))
+Value2 = .07633
+Value3 = 1000
+if value1 > 1000:
+    sum = ((value1 - Value3)*.09259) + (Value2 * Value3)
+else:
+    sum = value1 * Value2
+sum2 = (round(sum, 2))
+print("Amount owed is ${}".format(sum2))
+
+value1 = int(input("KW hours used:\n"))
+Value2 = .07633
+Value3 = 1000
+if value1 > 1000:
+    sum = ((value1 - Value3)*.09259) + (Value2 * Value3)
+else:
+    sum = value1 * Value2
+sum2 = (round(sum, 2))
+print("Amount owed is ${}".format(sum2))
+
+value1 = int(input("KW hours used:\n"))
+Value2 = .07633
+Value3 = 1000
+if value1 > 1000:
+    sum = ((value1 - Value3)*.09259) + (Value2 * Value3)
+else:
+    sum = value1 * Value2
+sum2 = (round(sum, 2))
+print("Amount owed is ${}".format(sum2))
+
+print("Marc Lebron")
