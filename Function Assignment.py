19c2e45d56ffbe33777f7faf8b2ad120df3c6515
MLebron1015 <mlebron20@mail.valenciacollege.edu>
2025-02-09 21:34:25 -0500

    Add files via upload

---
+++ Function Assignment.py
@@ -0,0 +1,90 @@
+#Area of Circle
+def areaofmycircle(r):
+    answer =  (round(3.141592653589793 * r*r, 2))
+    return answer, r
+
+A = areaofmycircle(10)
+print("Area of circle: ", A)
+
+def areaofmycircle(r):
+    answer =  (round(3.141592653589793 * r*r, 2))
+    return answer, r
+
+A = areaofmycircle(6)
+print("Area of circle: ", A)
+
+def areaofmycircle(r):
+    answer =  (round(3.141592653589793 * r*r, 2))
+    return answer, r
+
+A = areaofmycircle(24)
+print("Area of circle: ", A)
+
+def areaofmycircle(r):
+    answer =  (round(3.141592653589793 * r*r, 2))
+    return answer, r
+
+A = areaofmycircle(2)
+print("Area of circle: ", A)
+
+def areaofmycircle(r):
+    answer =  (round(3.141592653589793 * r*r, 2))
+    return answer, r
+
+A = areaofmycircle(1)
+print("Area of circle: ", A)
+
+#Taxes
+def taxes(m, r):
+    answer =  m+(m*r)
+    return answer,m, r
+
+T = taxes((20), (.06))
+print("taxed amount: ", T)
+
+#Taxes
+def taxes(m, r):
+    answer =  m+(m*r)
+    return answer,m, r
+
+T = taxes((58), (.04))
+print("taxed amount: ", T)
+
+#Taxes
+def taxes(m, r):
+    answer =  m+(m*r)
+    return answer,m, r
+
+T = taxes((68), (.08))
+print("taxed amount: ", T)
+
+#Temperature
+def temp(f):
+    answer =  (f-32)*(5/9)
+    return answer,f
+
+C = temp(32)
+print("conversion: ", C)
+
+def temp(f):
+    answer =  (round((f-32)*(5/9) ,4))
+    return answer,f
+
+C = temp(80)
+print("conversion: ", C)
+
+def temp(f):
+    answer =  (round((f-32)*(5/9) ,4))
+    return answer,f
+
+C = temp(73)
+print("conversion: ", C)
+
+def temp(f):
+    answer =  (round((f-32)*(5/9) ,4))
+    return answer,f
+
+C = temp(42)
+print("conversion: ", C)
+
+
