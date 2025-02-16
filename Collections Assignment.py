aa1c814c100a0162ec98a8f5196f493f9e2cad46
MLebron1015 <mlebron20@mail.valenciacollege.edu>
2025-02-16 17:57:51 -0500

    Add files via upload

---
+++ Collections Assignment.py
@@ -0,0 +1,111 @@
+print("Jeremiah")
+grades = ["47", "59", "93", "70", "89"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("Torrance")
+grades = ["94", "72", "91", "67", "100"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("Mary")
+grades = ["92", "44", "94", "83", "79"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("Beth")
+grades = ["77", "32", "27", "100", "92"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("John")
+grades = ["100", "100", "100", "99", "82"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("Larry")
+grades = ["44", "89", "77", "66", "100"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
+
+print("Megan")
+grades = ["50", "85", "75", "95", "95"]
+
+Average= (int((grades[0])) + int((grades[1])) + int((grades[2])) + int((grades[3])) + int((grades[4])))/5
+print("Average: {}".format(Average))
+if Average>=90:
+    print("A")
+if 90>Average>=80:
+    print("B")
+if 80>Average>=70:
+    print("C")
+if 70>Average>=60:
+    print("D")
+if 60>Average<60:
+    print("F")
